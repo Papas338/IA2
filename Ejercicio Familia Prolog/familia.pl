b20078b3c27eec4cb9b508c0b366306922854e6f
SantiYami <davidgarces.98@gmail.com>
2020-10-13 20:56:52 -0500

    Update familia.pl
    Probando si tiene sentido

--- Ejercicio Familia Prolog/familia.pl
+++ Ejercicio Familia Prolog/familia.pl
@@ -1,4 +1,11 @@
-
+:- dynamic ancestro/2.
+:- dynamic hombre/2.
+:- dynamic mujer/2.
+:- dynamic madre/2.
+:- dynamic padre/2.
+:- dynamic hermano/2.
+:- dynamic tia/2.
+:- dynamic tio/2.
 
 ancestro(flor,juana).
 ancestro(flor,maria).
@@ -44,3 +51,15 @@ padre(X,Y):-ancestro(X,Y),hombre(X).
 hermano(X,Y):-((madre(Z,X),madre(Z,Y));(padre(W,X),padre(W,Y))), X\=Y.
 tia(X,Y):-hermano(X,Z),ancestro(Z,Y),mujer(X).
 tio(X,Y):-hermano(X,Z),ancestro(Z,Y),hombre(X).
+
+% Comentarioxd
+
+undo :- ancestro(_ ,_),fail.
+undo :- hombre(_ ,_),fail.
+undo :- mujer(_ ,_),fail.
+undo :- madre(_ ,_),fail.
+undo :- padre(_ ,_),fail.
+undo :- hermano(_ ,_),fail.
+undo :- tia(_ ,_),fail.
+undo :- tio(_ ,_),fail.
+undo.
